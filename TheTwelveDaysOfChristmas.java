53fc97c14b01bae69ed6c2404e33b5336765588a
cleanjr <24brevanjr@gmail.com>
2025-10-13 14:44:49 -0400

    editted code for twelve days of christmas

--- TheTwelveDaysOfChristmas.java
+++ TheTwelveDaysOfChristmas.java
@@ -1,35 +1,33 @@
 public class TheTwelveDaysOfChristmas {
   public static void main(String[] args) {
-    String theSong;
-    String firstDay;
-    String secondDay;
-    String thirdDay;
-    String fourthDay;
-    String fifthDay;
-    String sixthDay;
-    String seventhDay;
-    String eighthDay;
-    String NinethDay;
-    String tenthDay;
-    String eleventhDay;
-    String twelvethDay;
-    String firstVerse = "On the first"
-    String secondVerse;
-    String thirdVerse;
-    String fourthVerse;
-    String fifthVerse;
-    Strig sixthVerse;
+    String dayOfChristmas;
 
     for(int i = 0; i < 12; i++){
       int j = 0;
       switch (i){
         case 0
-        theSong = firstDay;
-      }
-      switch (j){
-        system.out.println(theSong + firstVerse);
+        dayOfChristmas = "On the 1st of Christmas my true love gave to me";
+        case 1
+        dayOfChristmas = "On the 2nd of Christmas my true love gave to me";
+        case 2
+        dayOfChristmas = "On the 3rd of Christmas my true love gave to me";
+        case 3
+        dayOfChristmas = "On the 4th of Christmas my true love gave to me";
+        case 4
+        dayOfChristmas = "On the 5th of Christmas my true love gave to me";
+        case 5
+        dayOfChristmas = "On the 6th of Christmas my true love gave to me";
+        case 6 
+        dayOfChristmas = "On the 7th of Christmas my true love gave to me";
 
       }
+      switch (j){
+        case 0
+        system.out.println(dayOfChristmas + "\nA partige on a pear tree");
+
+      }
+      System.out.println(dayOfChristmas);
+      j++
     }
   }
 }
